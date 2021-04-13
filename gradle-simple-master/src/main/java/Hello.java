4203dab694b83a76c5e1ccd2098ccbd6a5ecc923
Ehsan <ehsan.bkh3@gmail.com>
2021-04-13 11:13:12 +0430

    Two

--- gradle-simple-master/src/main/java/Hello.java
+++ gradle-simple-master/src/main/java/Hello.java
@@ -2,7 +2,8 @@
 public class Hello {
   
   public static String GREETING = "Hello world!";
-  
+  public static String GREETING2 = "Hello world2!";
+
   public static void main(String []args) {
     
   }  
